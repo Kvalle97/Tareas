219058a19ffbc12f7fa8aa611756e695051bf129
KeynerV <Keyner18@gmail.com>
2022-06-23 14:43:06 -0600

    Tarea

---
+++ Recordando/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Recordando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = 0;
+            resultado = suma(1, 2, 3);
+            Console.WriteLine(resultado);
+
+
+            coche micoche = new coche();
+            micoche.Puertas();
+            Console.WriteLine(micoche.puertas);
+
+        }
+
+        public static int suma(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+
+        class coche
+        {
+            public int puertas = 4;
+
+            public void Puertas()
+            {
+                this.puertas++;
+            }
+        }
+
+
+    }
+}
+
